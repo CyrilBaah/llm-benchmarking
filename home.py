8907fd5f99239ff53443ecc3dd73e6bc8a8d4993
CyrilBaah <baah_yard12@yahoo.com>
2026-01-16 21:39:51 +0000

    Function to call GenAI model

--- home.py
+++ home.py
@@ -3,9 +3,26 @@ from google import genai
 import time
 import pandas as pd
 import plotly.express as px
+from groq import Groq
 
 st.set_page_config(page_title="LLM Benchmarking", layout="wide")
 st.title("LLM Benchmarking Dashboard")
 st.subheader("Compare as many LLMs as you want")
 st.divider()
+
+client = genai.Client(api_key="")
+groq_client = Groq(api_key="")
+
+# Function to call GenAI model
+def call_genai_model(model_name, prompt):
+    start = time.time()
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=prompt )
+    end = time.time()
+    
+    if response.usage_metadata:
+        tokens_used = response.usage_metadata.total_tokens
+    else:
+        tokens_used = len(response.text) // 4
+    return response.text, end - start, tokens_used
 
